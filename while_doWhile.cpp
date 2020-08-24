61644ffce9e94b26eb070d322af84102fa8d4261
AshTiwari <ashu.atiwa@gmail.com>
2020-08-24 16:53:31 +0530

    while loop

---
+++ while_doWhile.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int index = 1;
+    while(index<=5)
+    {
+        cout<<index<<endl;
+        index++;
+    }
+    do
+    {
+        cout<<index<<endl;
+    } while (index<=5);
+    return 0;
+}
